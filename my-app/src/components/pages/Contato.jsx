fdd5b15e96664766d5f0244a1952e14e14995141
Andson <andsonsilva2015@hotmail.com>
2022-06-16 16:13:08 -0300

    pages Contato e Localização finalizadas

--- my-app/src/components/pages/Contato.jsx
+++ my-app/src/components/pages/Contato.jsx
@@ -1,8 +1,46 @@
+import styles from './Contato.module.css';
+import imgContato from '../../image/Contato.png'
+
+import { FaWhatsapp, FaPhone } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+
 function Contato() {
     return (
-        <div>
-            Contato
-        </div>
+        <section className={styles.section}>
+
+            <img className={styles.imgContato} src={imgContato} alt="" />
+
+            <h1>Entre em contato através de nosso whatsapp</h1>
+            
+            <a className={styles.wpp} href="https://api.whatsapp.com/send/?phone=5581981952775&text&app_absent=0" target="_blank" rel="noreferrer">
+                    <FaWhatsapp className={styles.icon} /> (81)981952775
+            </a>
+            <a className={styles.wpp} href="https://api.whatsapp.com/send/?phone=5581981593273&text&app_absent=0" target="_blank" rel="noreferrer">
+                    <FaWhatsapp className={styles.icon} /> (81)981593273
+            </a>
+
+            <ul className={styles.balao}>
+
+                <li className={styles.telefone}>
+
+                    <p><FaPhone /> Telefones para contato:</p>
+
+                        <a className={styles.link} href="https://api.whatsapp.com/send/?phone=5581981952775&text&app_absent=0" target="_blank" rel="noreferrer">
+                            <p className={styles.azul}>(81)981952775</p>
+                        </a>
+                        <a className={styles.link} href="https://api.whatsapp.com/send/?phone=5581981593273&text&app_absent=0" target="_blank" rel="noreferrer">
+                            <p className={styles.azul}>(81)981593273</p>
+                        </a>
+                </li>
+                
+                <li className={styles.email}>
+                    <p><MdEmail /> E-mail para contato:</p>
+
+                        <p className={styles.azul}>recantodosol@gmail.com.br</p>
+                </li>
+
+            </ul>
+        </section>
     );
 }
 
--- my-app/src/components/pages/Localizacao.jsx
+++ my-app/src/components/pages/Localizacao.jsx
@@ -1,8 +1,18 @@
+import styles from './Localizacao.module.css';
+
+import imgLoca from '../../image/Localização.png'
+
 function Localizacao() {
     return (
-        <div>
-            Localizacao
-        </div>
+        <section className={styles.section}>
+
+            <img className={styles.imgLoca} src={imgLoca} alt="" />
+
+            <h1>Localização</h1>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6763.589060868303!2d-35.847669790323316!3d-7.7741326567700035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5f1be98389279dff!2sRecanto%20do%20Sol!5e1!3m2!1spt-BR!2sbr!4v1655405818617!5m2!1spt-BR!2sbr"
+             style={{ width: '800px', height:'500px', border:'0', padding: '50px'}} title='Mapa' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
     );
 }
 
